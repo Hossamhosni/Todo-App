9edad8cb792cb51bef14b4b5158b5a13090abe1e
Hossam <jkasdhkjadas>
2017-05-12 05:51:51 +0200

    Add GET route to get all todos

--- server/server.js
+++ server/server.js
@@ -11,15 +11,24 @@ var {User} = require('./models/user');
 // setting app and the port settings
 var app = express();
 app.set('port', process.env.PORT || 3000);
-
 app.use(bodyParser.json());
 
+// Post route for adding todos to the database
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
     todo.save().then((doc) => {
         res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+//GET route for getting the todos from the database
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
     }, (e) => {
         res.status(400).send(e);
     });
