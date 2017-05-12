6d9d2793a3bfa8ab7b7ff7c7df7a90b7eac5b6fb
Hossam <jkasdhkjadas>
2017-05-12 05:13:22 +0200

    Add Post todo route

---
+++ server/server.js
@@ -0,0 +1,32 @@
+'use strict';
+// third party imports
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// local imports
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+// setting app and the port settings
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+// listen to the port
+app.listen(app.get('port'), () => {
+    console.log("Started on port 3000");
+});
+
