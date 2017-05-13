7792a7b2b549ba97bbf74e6f343aaddb5207f443
Hossam <jkasdhkjadas>
2017-05-13 23:50:35 +0200

    Fixing Bug

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/27017");
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/27017/TodoApp");
 
 module.exports = {
     mongoose
