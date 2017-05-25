362c4617480a1864266c590a3984a8f2c8d4c0d8
Hossam <jkasdhkjadas>
2017-05-25 04:52:55 +0200

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -134,6 +134,12 @@ app.post('/users/login', (req, res) => {
   });
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  });
+});
+
 // listen to the port
 app.listen(app.get('port'), () => {
     console.log(`Started on port ${app.get('port')}`);
