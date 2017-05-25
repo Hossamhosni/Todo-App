fa1ef68e98edb1da302413f0890b90fea5d0cca6
Hossam <jkasdhkjadas>
2017-05-25 04:16:06 +0200

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ if (env === "development") {
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 const {ObjectID} = require('mongodb');
 
 // local imports
@@ -120,6 +121,17 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 // listen to the port
