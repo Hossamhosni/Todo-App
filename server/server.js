4a2ebcec6fb6cd5f98956c0d452fe44c9d9e19c0
Hossam <jkasdhkjadas>
2017-05-25 06:11:10 +0200

    Update config

--- server/server.js
+++ server/server.js
@@ -1,14 +1,7 @@
 'use strict';
 
 //config
-var env = process.env.NODE_ENV || 'development';
-if (env === "development") {
-  process.env.PORT = 3000;
-  process.env.MONGOLAB_URI = "mongodb://localhost:27017/TodoApp";
-} else if (env === "test") {
-  process.env.PORT = 3000;
-  process.env.MONGOLAB_URI = "mongodb://localhost:27017/TodoAppTest";
-}
+require('./config/config');
 // imports
 const _ = require('lodash');
 const express = require('express');
